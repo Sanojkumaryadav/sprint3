537486e92e71f66e01a653df204b8e589f5c9ef6
Sanoj Kumar <sanojsanoj638@gmail.com>
2022-06-20 21:54:29 +0530

    almost done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { AuthContext } from "../context/AuthContext";
+import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
-  return <div>{/* CODE HERE */}</div>;
+
+  const { isAuth, handleAuth } = React.useContext(AuthContext);
+  const { cartCount, handleCart } = React.useContext(CartContext);
+
+  return <div style={{gap:'50px', display:'flex', justifyContent:'center'}}>
+    {
+      isAuth ? <Link to="/home">Home</Link> : <p style={{margin:'auto', marginRight:0,marginLeft:0}}>Home</p>
+    }
+    {
+      isAuth ? <p style={{margin:'auto', marginRight:0,marginLeft:0}}>Cart : {cartCount}</p> : null
+    }
+    {
+      isAuth ? <button onClick={handleAuth}>Logout</button> : <Link to="/">Login</Link>
+    }
+    </div>;
 };
 
 export default Navbar;
